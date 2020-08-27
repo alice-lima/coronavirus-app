fc05f7d1e1cd8f1d198abcb562f474432ffc836a
Alice Lima <alice_lima2@live.com>
2020-08-27 09:53:54 -0300

    searchInput

---
+++ src/components/SearchInput/index.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+function SearchInput(props) {
+  const { countries } = props;
+
+  const [displayedCountries, setDisplayedEmpresas] = useState(countries);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    function hasParamMatch(countrieParam) {
+      const { countrie } = countrieParam;
+      const searchValueLowerCase = searchValue.toLocaleLowerCase();
+      return (
+        searchValueLowerCase === "" ||
+        countrie.toLowerCase().includes(searchValueLowerCase)
+      );
+    }
+    if (searchValue !== "") {
+      const toDisplay = countries.filter((countrie) => hasParamMatch(countrie));
+      setDisplayedEmpresas(toDisplay);
+    } else {
+      setDisplayedEmpresas(countries);
+    }
+  }, [searchValue, countries]);
+
+  return <input onChange={(evt, data) => setSearchValue(data.value)} />;
+}
